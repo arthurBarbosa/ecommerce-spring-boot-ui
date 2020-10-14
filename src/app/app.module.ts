3df0b9b9422b1458869b7939cdb8f7ab3b91aa11
Arthur Barbosa <arthur.barbosa@itbam.org.br>
2020-10-14 15:08:33 -0400

    config file routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProdutcListComponent } from './components/produtc-list/produtc-list.component';
 import { ProductService } from './services/product.service';
+
+const routes: Routes = [
+  {path: 'category/:id', component: ProdutcListComponent},
+  {path: 'category', component: ProdutcListComponent},
+  {path: 'products', component: ProdutcListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -11,6 +20,7 @@ import { ProductService } from './services/product.service';
     ProdutcListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
