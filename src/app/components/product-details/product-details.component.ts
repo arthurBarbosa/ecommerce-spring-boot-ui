948475bc72b3f03ef95eeca8b0af0c8e92527bde
Arthur Barbosa <arthur.barbosa@itbam.org.br>
2020-10-20 08:17:52 -0400

    addToCart from product details view

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
+import { CartService } from 'src/app/services/cart.service';
+import { CartItem } from 'src/app/common/cart-item';
 
 
 @Component({
@@ -14,6 +16,7 @@ export class ProductDetailsComponent implements OnInit {
   product: Product =  new Product();
 
   constructor(private productService: ProductService,
+              private  cartService: CartService,
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -32,6 +35,13 @@ export class ProductDetailsComponent implements OnInit {
         this.product = data;
       }
     )
+  }
+
+  addToCart(){
+
+    console.log(`Product ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 
 }
