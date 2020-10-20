28e42fdac7461f6599ec60bd4d3302023982f3cc
Arthur Barbosa <arthur.barbosa@itbam.org.br>
2020-10-20 11:23:39 -0400

    adedd method increment product in cart-details

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -38,5 +38,8 @@ export class CartDetailsComponent implements OnInit {
     this.cartService.computeCartTotals();
   }
   
+  incrementQuantity(theCartItem: CartItem){
+    this.cartService.addToCart(theCartItem);
+  }
 
 }
