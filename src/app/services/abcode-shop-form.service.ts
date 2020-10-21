5879f504bc7499176ef37680bf6ec047370c590c
Arthur Barbosa <arthur.barbosa@itbam.org.br>
2020-10-21 11:22:55 -0400

    create class country and state

--- src/app/services/abcode-shop-form.service.ts
+++ src/app/services/abcode-shop-form.service.ts
@@ -1,12 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Country } from '../common/country';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AbcodeShopFormService {
 
-  constructor() { }
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statesUrl = 'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCountries(): Observable<Country[]> {
+   
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountyCode: string): Observable<State[]>{
+
+    // search url
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountries?code=${theCountyCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    )
+  }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
 
@@ -37,5 +61,17 @@ export class AbcodeShopFormService {
     }
 
     return of(data);
+  }
+}
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates{
+  _embedded:{
+    state: State[];
   }
 }
