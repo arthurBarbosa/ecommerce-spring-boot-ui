446fc92f418cae5c31eb2411d136567a440fd930
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-04-06 16:44:49 -0400

    Add okta sdk

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oajx2lsufJHnCCU95d6',
+    issuer: 'https://dev-50742809.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
